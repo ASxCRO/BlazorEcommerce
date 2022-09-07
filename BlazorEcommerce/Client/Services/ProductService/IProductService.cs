240e81a8a9ef97fd74edc0f58b3b8e3818ce3058
ASxCRO <antonio.suups@gmail.com>
2022-09-07 14:58:14 +0200

    feat: search frontend logic

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -3,9 +3,11 @@
     public interface IProductService
     {
         event Action ProductsChanged;
+        string Message { get; set; }
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
-
-        Task<ServiceResponse<Product>> GetProductAsync(int productId);  
+        Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task SearchProducts(string searchText);
+        Task<List<string>> GetProductSearchSuggestions(string searchText);
     }
 }
