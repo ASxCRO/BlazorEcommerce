1e5424ecd7d69c4cb917613af13d8cba81c37eb0
ASxCRO <antonio.suups@gmail.com>
2022-09-09 09:04:00 +0200

    feat: localstorage setup for cart, cart ui on layout and cart service on client

---
+++ BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -0,0 +1,43 @@
+﻿using BlazorEcommerce.Shared;
+using Blazored.LocalStorage;
+
+namespace BlazorEcommerce.Client.Services.CartService
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public event Action OnChange;
+
+        public CartService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async Task AddToCart(CartItem cartItem)
+        {
+            List<CartItem>? cart = await GetCart();
+
+            cart.Add(cartItem);
+
+            await _localStorage.SetItemAsync("cart", cart);
+        }
+
+        public async  Task<List<CartItem>> GetCartItems()
+        {
+            return await GetCart();
+        }
+
+        private async Task<List<CartItem>> GetCart()
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+
+            if (cart == null)
+            {
+                cart = new();
+            }
+
+            return cart;
+        }
+    }
+}
